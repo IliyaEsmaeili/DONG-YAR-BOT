417df2c5cc23aa3ea655e57207c0e2cbd5ac55ae
Iliya Esmaeili <iliya.esmaeili.u@gmail.com>
2026-06-01 01:32:35 +0330

    swiched to telegram only from now -- bale wont be my focus but the code works probably fine for both maybe a little bit of change might be required

--- src/bot_instance.py
+++ src/bot_instance.py
@@ -6,8 +6,8 @@ import telebot
 load_dotenv()
 BALE_TOKEN = os.getenv("BALE_BOT_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-if BALE_TOKEN is None:
+if TELEGRAM_TOKEN is None:
     raise ValueError("BALE_BOT_TOKEN is missing! Check your .env file.")
 
 apihelper.API_URL = "https://tapi.bale.ai/bot{0}/{1}"
-bot = telebot.TeleBot(BALE_TOKEN)
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
