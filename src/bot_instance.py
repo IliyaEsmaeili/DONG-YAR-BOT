a8b38c8043e0262fd4ed435b443eb472ed24e551
Iliya Esmaeili <iliya.esmaeili.u@gmail.com>
2026-06-01 02:01:42 +0330

    fixed base api for telegram

--- src/bot_instance.py
+++ src/bot_instance.py
@@ -7,7 +7,7 @@ load_dotenv()
 BALE_TOKEN = os.getenv("BALE_BOT_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if TELEGRAM_TOKEN is None:
-    raise ValueError("BALE_BOT_TOKEN is missing! Check your .env file.")
+    raise ValueError("TELEGRAM_BOT_TOKEN is missing! Check your .env file.")
 
-apihelper.API_URL = "https://tapi.bale.ai/bot{0}/{1}"
+# apihelper.API_URL = "https://tapi.bale.ai/bot{0}/{1}"
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
