d2bf55e00adbbcd4c4cf9f1d1aaf62082bdeb29b
Iliya Esmaeili <iliya.esmaeili.u@gmail.com>
2026-07-21 22:07:27 +0330

    created helper methods to execute and fetch to and from data base

---
+++ src/database/repositories.py
@@ -0,0 +1,15 @@
+from connection import conn
+
+def execute_query(query , params) :
+    with conn.cursor() as cursor :
+        cursor.execute(query , params)
+
+def fetch_one(query , params) :
+    with conn.cursor() as cursor :
+        cursor.execute(query , params)
+        return cursor.fetchone()
+
+def fetch_all(query , params) :
+    with conn.cursor() as cursor :
+        cursor.execute(query , params)
+        return cursor.fetchall()
